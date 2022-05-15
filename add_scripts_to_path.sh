c0edb0c05ec2daf8b9c88f68636ccfd48f287e5e
Jason Galea <jasonpgalea@outlook.com.au>
2022-05-15 13:24:09 +1000

    Added script to add other scripts to /home/jg/.local/bin:/home/jg/.cargo/bin:/home/jg/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/mnt/c/Windows/system32:/mnt/c/Windows:/mnt/c/Windows/System32/Wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/:/mnt/c/Windows/System32/OpenSSH/:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/Program Files/NVIDIA Corporation/NVIDIA NvDLISR:/mnt/c/Program Files/Microsoft SQL Server/110/Tools/Binn/:/mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/ProgramData/DockerDesktop/version-bin:/mnt/c/Program Files/PowerShell/7/:/mnt/c/Program Files/Git/cmd:/mnt/c/Users/jg/.cargo/bin:/mnt/c/Users/jg/AppData/Local/Programs/Python/Python39/Scripts/:/mnt/c/Users/jg/AppData/Local/Programs/Python/Python39/:/mnt/c/Users/jg/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/jg/AppData/Local/Programs/ffmpeg-N-102852-g758e2da289-win64-gpl/bin:/mnt/c/Users/jg/AppData/Local/Programs/chromedriver_win32/:/mnt/c/Program Files (x86)/Nmap:/mnt/c/php:/mnt/c/Users/jg/AppData/Local/Programs/terraform_1.1.5_windows_amd64:/mnt/c/Users/jg/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin

--- add_scripts_to_path.sh
+++ add_scripts_to_path.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/bash
 
 
-# ### Force root???
-# if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-#     echo "Please run script as root"
-#     exit
-# fi
+BIN="$HOME/.local/bin"
+ME=`basename "$0"`
 
 
-### Check/create ~/.local/bin/misc_scripts
+
+### Create symbolic links
+for s in $(ls *.sh | grep -v "$ME" | sed 's/.sh//'); do
+    echo "$ ln -s $s.sh $BIN/$s"
+    ln -s $(pwd)/$s.sh $BIN/$s
+    echo
+done
+
+echo "$ ls -lh $BIN"
+ls -lh $BIN
+
+echo "Symbolic links created, please restart your shell"
 
 
-### Set permissions on local bin
 
-
-### For each script "*.sh" in misc_scripts:
-### ln -s $script ~/.local/bin/misc_scripts/$($script.strip(".sh")) ???
