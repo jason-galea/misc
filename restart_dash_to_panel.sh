31073ca819fa6d406756b55a0cee3fbac100c57f
jg <jg@pop-os.localdomain>
2023-01-10 13:55:51 +0700

    Removed sleep time from dash-to-panel

--- restart_dash_to_panel.sh
+++ restart_dash_to_panel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 10
+#sleep 40
 gnome-extensions disable dash-to-panel@jderose9.github.com --quiet
-sleep 5
+#sleep 5
 gnome-extensions enable dash-to-panel@jderose9.github.com --quiet
 
 
