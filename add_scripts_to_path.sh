d2bf1d2c2a83fb9e477d0151109626cb4c67e8e0
jg <jg@pop-os.localdomain>
2023-01-11 20:07:56 +0700

    Fixed add_script_to_path script

--- add_scripts_to_path.sh
+++ add_scripts_to_path.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 
-### Vars
 BIN_DIR="$HOME/.local/bin"
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 
-### Create bin dir/Delete old symlinks
+### Delete symlinks OR create bin dir
 if [[ -d $BIN_DIR ]]; then
     echo -e "\n==> Deleting old symlinks"
     for SYMLINK in $(find $BIN_DIR -type l); do ### Deals with 0 results
@@ -16,16 +15,17 @@ else
     mkdir $BIN_DIR
 fi
 
+
 ### Create
 echo -e "\n==> Creating new symlinks"
 for SCRIPT in $(find $SCRIPT_DIR -type f -iname "*.sh"); do
-    echo "$ ln -s $SCRIPT $BIN_DIR/$(basename $SCRIPT .sh)"
+    echo "ln -s $SCRIPT $BIN_DIR/$(basename $SCRIPT .sh)"
     ln -s $SCRIPT $BIN_DIR/$(basename $SCRIPT .sh)
 done
 
+
 ### Signpost
-echo -e "\n==> Displaying symlinks"
+echo -e "\n==> Displaying symlinks in '$BIN_DIR'"
 ls -lh $BIN_DIR
 
-echo -e "\n==> Symlinks created"
-echo -e "\n==> You can now execute the above scripts as commands"
+echo -e "\n==> SUCCESS! (hopefully? :O)"
